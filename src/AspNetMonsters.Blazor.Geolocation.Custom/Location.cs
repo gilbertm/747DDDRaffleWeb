1416891a143d003fadcea04a8b499cbd00620251
Gilbert PC <gilbert_maloloyon@outlook.com>
2022-09-10 14:06:33 +0400

    ehulog client v1.0 - custom project needs to incorporated in the container

---
+++ src/AspNetMonsters.Blazor.Geolocation.Custom/Location.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetMonsters.Blazor.Geolocation.Custom
+{
+    public class Location
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Accuracy { get; set; }
+
+        public override string ToString()
+        {
+            return $"Location: ({Latitude}, {Longitude}) with accuracy {Accuracy}";
+        }
+    }
+}
---
+++ src/AspNetMonsters.Blazor.Geolocation.Custom/src/Location.ts
@@ -0,0 +1,45 @@
+﻿declare var DotNet: any;
+
+class coordinate {
+    public latitude: number = 0;
+    public longitude: number = 0;
+    public accuracy: number = 0;
+}
+
+
+let assemblyName = "AspNetMonsters.Blazor.Geolocation";
+let namespace = "AspNetMonsters.Blazor.Geolocation";
+let type = "LocationService";
+async function dispatchResponse(id: string, location: coordinate) {
+    await DotNet.invokeMethodAsync(namespace, 'ReceiveResponse', id, location.latitude, location.longitude, location.accuracy);
+}
+
+async function dispatchWatchResponse(id: string, location: coordinate) {
+    await DotNet.invokeMethodAsync(namespace, 'ReceiveWatchResponse', id, location.latitude, location.longitude, location.accuracy);
+}
+
+window['AspNetMonsters'] = window['AspNetMonsters'] || {};
+window['AspNetMonsters']['Blazor'] = window['AspNetMonsters']['Blazor'] || {};
+window['AspNetMonsters']['Blazor']['Geolocation'] = window['AspNetMonsters']['Blazor']['Geolocation'] || {};
+
+window['AspNetMonsters']['Blazor']['Geolocation']['GetLocation'] = (requestId) => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            dispatchResponse(requestId, position.coords);
+        });
+    }
+    else {
+        return "No location finding";
+    }
+};
+
+window['AspNetMonsters']['Blazor']['Geolocation']['WatchLocation'] = (requestId) => {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition((position) => {
+            dispatchWatchResponse(requestId, position.coords);
+        });
+    }
+    else {
+        return "No location watching";
+    }
+};
