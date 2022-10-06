da2bc446845d08b0ec9399e7b963713d0ae00ce5
Gilbert PC <gilbert_maloloyon@outlook.com>
2022-10-07 02:39:16 +0400

    ehulog client v1.1 - static pages; sliders; layouts.

---
+++ src/Client/Pages/Contact.razor.cs
@@ -0,0 +1,59 @@
+﻿using EHULOG.BlazorWebAssembly.Client.Components.Common;
+using EHULOG.BlazorWebAssembly.Client.Infrastructure.ApiClient;
+using EHULOG.BlazorWebAssembly.Client.Shared;
+using EHULOG.WebApi.Shared.Multitenancy;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace EHULOG.BlazorWebAssembly.Client.Pages;
+
+public partial class Contact
+{
+    private readonly CreateUserRequest _createUserRequest = new();
+    private CustomValidation? _customValidation;
+
+    private bool BusySubmitting { get; set; }
+
+    [Inject]
+    private IUsersClient UsersClient { get; set; } = default!;
+
+    private string Tenant { get; set; } = MultitenancyConstants.Root.Id;
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    private async Task SubmitAsync()
+    {
+        BusySubmitting = true;
+
+        string? sucessMessage = await ApiHelper.ExecuteCallGuardedAsync(
+            () => UsersClient.SelfRegisterAsync(Tenant, _createUserRequest),
+            (ISnackbar)Snackbar,
+            _customValidation);
+
+        if (sucessMessage != null)
+        {
+            Snackbar.Add(sucessMessage, Severity.Info);
+            Navigation.NavigateTo("/login");
+        }
+
+        BusySubmitting = false;
+    }
+
+    private void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
