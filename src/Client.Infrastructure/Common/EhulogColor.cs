e460dbcca8511113a5f745b0d81e98244a190df5
Gilbert PC <gilbert_maloloyon@outlook.com>
2022-11-01 19:25:40 +0400

    ehulog 2.0 - beta 0.1 -  colors, icons, sass styles

---
+++ src/Client.Infrastructure/Common/EhulogColor.cs
@@ -0,0 +1,10 @@
+namespace EHULOG.BlazorWebAssembly.Client.Infrastructure.Common;
+
+public static class EhulogColor
+{
+    public const string GreenMoney = "#1B8366";
+    public const string GreenMoneyLight = "#57C478";
+    public const string Yellow = "#FFDD0E";
+    public const string Orange = "#E9AE0B";
+    public const string BlueLink = "#268bd2";
+}
