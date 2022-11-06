04f5c206c73154fee278204f6c93f5f11c9d873e
Gilbert PC <gilbert_maloloyon@outlook.com>
2022-11-07 01:00:49 +0400

    ehulog 2.0 | test - beta 0.1 -  added non implemented can create business logic. cleanup

--- src/Client/Shared/AppDataService.cs
+++ src/Client/Shared/AppDataService.cs
@@ -117,7 +117,12 @@ public class AppDataService : IAppDataService
             {
                 // if not found / not found exception
                 // create a silent custom helper
-                if (await ApiHelper.ExecuteCallGuardedCustomAsync(async () => await AppUsersClient.GetAsync(userId), Snackbar, default) == false)
+                var appUserClient = await ApiHelper.ExecuteCallGuardedCustomSuppressAsync(async () => await AppUsersClient.GetAsync(userId), Snackbar, default);
+
+                // the get async is throwing errors
+                // the user doesn't not exist
+                // create init
+                if (appUserClient == default)
                 {
                     var createAppUserRequest = new CreateAppUserRequest
                     {
@@ -130,6 +135,7 @@ public class AppDataService : IAppDataService
                     }
                 }
 
+                // the app user should be present here
                 AppUser = await ApiHelper.ExecuteCallGuardedAsync(async () => await AppUsersClient.GetAsync(userId), Snackbar, default!) ?? default!;
 
                 if (AppUser != default)
@@ -151,66 +157,70 @@ public class AppDataService : IAppDataService
                                 EndpointType = Geo.MapBox.Enums.EndpointType.Places
                             });
 
-                            if (responseReverseGeocoding is { })
+                            if (responseReverseGeocoding != default)
                             {
-                                if (responseReverseGeocoding.Features.Count > 0)
+                                if (responseReverseGeocoding.Features != default)
                                 {
-                                    foreach (var f in responseReverseGeocoding.Features)
+                                    if (responseReverseGeocoding.Features.Count > 0)
                                     {
-                                        if (f.Contexts.Count > 0)
+                                        foreach (var f in responseReverseGeocoding.Features)
                                         {
-                                            foreach (var c in f.Contexts)
+                                            if (f.Contexts.Count > 0)
                                             {
-                                                // System.Diagnostics.Debug.Write(c.Id.Contains("Home"));
-                                                // System.Diagnostics.Debug.Write(c.ContextText);
-
-                                                if (!string.IsNullOrEmpty(c.Id))
+                                                foreach (var c in f.Contexts)
                                                 {
-                                                    switch (c.Id)
+                                                    // System.Diagnostics.Debug.Write(c.Id.Contains("Home"));
+                                                    // System.Diagnostics.Debug.Write(c.ContextText);
+
+                                                    if (!string.IsNullOrEmpty(c.Id))
                                                     {
-                                                        case string s when s.Contains("country"):
-                                                            AppUser.HomeCountry = c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("region"):
-                                                            AppUser.HomeRegion = c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("postcode"):
-                                                            AppUser.HomeAddress += c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("district"):
-                                                            AppUser.HomeCountry += c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("place"):
-                                                            AppUser.HomeCity = c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("locality"):
-                                                            AppUser.HomeAddress += c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("neighborhood"):
-                                                            AppUser.HomeAddress += c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("address"):
-                                                            AppUser.HomeAddress += c.ContextText[0].Text;
-                                                            break;
-                                                        case string s when s.Contains("poi"):
-                                                            AppUser.HomeAddress += c.ContextText[0].Text;
-                                                            break;
+                                                        switch (c.Id)
+                                                        {
+                                                            case string s when s.Contains("country"):
+                                                                AppUser.HomeCountry = c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("region"):
+                                                                AppUser.HomeRegion = c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("postcode"):
+                                                                AppUser.HomeAddress += c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("district"):
+                                                                AppUser.HomeCountry += c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("place"):
+                                                                AppUser.HomeCity = c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("locality"):
+                                                                AppUser.HomeAddress += c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("neighborhood"):
+                                                                AppUser.HomeAddress += c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("address"):
+                                                                AppUser.HomeAddress += c.ContextText[0].Text;
+                                                                break;
+                                                            case string s when s.Contains("poi"):
+                                                                AppUser.HomeAddress += c.ContextText[0].Text;
+                                                                break;
+                                                        }
                                                     }
                                                 }
                                             }
-                                        }
 
-                                        if (f.Center is { })
-                                        {
-                                            AppUser.Latitude = f.Center.Latitude.ToString();
-                                            AppUser.Longitude = f.Center.Longitude.ToString();
-                                        }
+                                            if (f.Center is { })
+                                            {
+                                                AppUser.Latitude = f.Center.Latitude.ToString();
+                                                AppUser.Longitude = f.Center.Longitude.ToString();
+                                            }
 
-                                        if (f.Properties.Address is { })
-                                        {
-                                            AppUser.HomeAddress += f.Properties.Address;
+                                            if (f.Properties.Address is { })
+                                            {
+                                                AppUser.HomeAddress += f.Properties.Address;
+                                            }
                                         }
                                     }
+
                                 }
                             }
                         }
@@ -319,7 +329,14 @@ public class AppDataService : IAppDataService
     }
 
     /*                                                      ------ Business Logics ------                                                       */
-    //TODO:// business logics
+
+    // TODO:// business logics
+
+    public bool IsCanCreateLoan()
+    {
+        // TODO: to be implemented
+        return true;
+    }
 
     public bool IsLesseeOfThisLoan(LoanDto Loan)
     {
@@ -362,7 +379,7 @@ public class AppDataService : IAppDataService
     {
         bool isAnApplicantOfThisLoan = false;
         bool isLessee = false;
-        bool isApplicantFlagNormal = false;
+        // bool isApplicantFlagNormal = false;
 
         if (AppUser != default)
         {
@@ -469,13 +486,59 @@ public class AppDataService : IAppDataService
         return false;
     }
 
-    /*                                                      ------ Business Logics ------                                                       */
+    public async Task RevalidateVerification(AppUserDto overrideAppUser = default!)
+    {
+        // if appuser is provided
+        // this process can be used by admins
+        // revalidating user
+        if (overrideAppUser != default)
+        {
+            AppUser = overrideAppUser;
+        }
+
+        if (AppUser != default)
+        {
+            // appuser get uses the main platform application user identification
+            if (await ApiHelper.ExecuteCallGuardedAsync(async () => await AppUsersClient.GetAsync(AppUser.ApplicationUserId), Snackbar, null) is AppUserDto appUser)
+            {
+                if (appUser != default)
+                {
+                    // revalidate the verification status of the user
+                    // stages that needs fulfillment
+                    // a. roles and packages
+                    // b. documents
+                    // c. address
+                    // await AppDataService.RevalidateVerification(AppDataService.AppUser.ApplicationUserId);
+
+                    // appuser get uses the main platform application user identification
+                    if (appUser.DocumentsStatus.Equals(VerificationStatus.Verified) && appUser.RolePackageStatus.Equals(VerificationStatus.Verified) && appUser.AddressStatus.Equals(VerificationStatus.Verified))
+                    {
+                        var updateAppUserRequest = new UpdateAppUserRequest
+                        {
+                            Id = AppUser.Id,
+                            ApplicationUserId = AppUser.ApplicationUserId,
+                            IsVerified = true
+                        };
+
+                        // appuser update uses the extended app user mapping id
+                        if (await ApiHelper.ExecuteCallGuardedAsync(async () => await AppUsersClient.UpdateAsync(AppUser.Id, updateAppUserRequest), Snackbar, null) is Guid guid)
+                        {
+                            // appuser get uses the main platform application user identification
+                            // update the the service appuser info
+                            AppUser = await ApiHelper.ExecuteCallGuardedAsync(async () => await AppUsersClient.GetAsync(AppUser.ApplicationUserId), Snackbar, null) ?? default!;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /*                                                      ------ /Business Logics ------                                                       */
 
     [JSInvokable]
     public void OnPositionRecieved(GeolocationPosition position)
     {
         _position = position;
-
         NotifyDataChanged();
     }
 
