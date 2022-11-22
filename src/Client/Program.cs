e6e67be39a8ad7f39e4ef3bd42981a8e8f4b84e6
Gilbert PC <gilbert_maloloyon@outlook.com>
2022-11-22 22:03:21 +0400

    ehulog 2.0 - remove blazor local storage webassembly. use the blazored storage included with base

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -15,7 +15,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddClientServices(builder.Configuration);
 builder.Services.AddGeolocationServices();
-builder.Services.AddLocalStorageServices();
 builder.Services.AddMapBoxServices(options => options.UseKey(builder.Configuration["MapBox:Key"]));
 builder.Services.AddTransient<IAppDataService, AppDataService>();
 builder.Services.AddScoped<AppDataService>();
